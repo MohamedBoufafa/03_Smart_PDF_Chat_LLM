8c0910319100f60e6620cf4d8ab20e7634e9409b
zinou123viva <zinou123viva@gmail.com>
2025-10-18 11:47:33 +0100

    Improve: Better answers, fix filename display

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def load_models():
         embedding_model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
         
         # LLM model (Flan-T5 for speed)
-        model_name = 'google/flan-t5-large'
+        model_name = 'google/flan-t5-base'
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForSeq2SeqLM.from_pretrained(
             model_name,
